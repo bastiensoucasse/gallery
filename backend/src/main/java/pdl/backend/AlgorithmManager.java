58d5a9fa7f9d353fde67423b2a0dc54b245ebfe3
Gilles <gsouton@gmail.com>
2021-03-22 12:26:57 +0100

    Add explanation in Algorithm manager

--- backend/src/main/java/pdl/backend/AlgorithmManager.java
+++ backend/src/main/java/pdl/backend/AlgorithmManager.java
@@ -17,6 +17,18 @@ import net.imglib2.type.numeric.integer.UnsignedByteType;
 import pdl.processing.ImageConverter;
 import pdl.processing.Processing;
 
+/** Explication :
+    1) je récupère le nom de l'algorithme et ses paramètres dans l'URL  et stocke tout ça dans une Map<String, String> 
+    2) j'ai créer une classe AlgorithmManager qui implémente un singleton, cette classe référence toutes les methodes (String) associé avec leur Class<?> ``` Map<String, Class<?>> algorithms ```
+    ainsi que chaque type de chaque paramètres pour chaque méthodes ``` Map<String, ArrayList<Class<?>[]>> ```
+    3) Je parse les arguments pour que ça correspond a la liste de type pour la méthode associé et génère une liste d'arguments ```Object[] arguments ```
+    4) Je récupère la méthode avec sa classe ``` Method m = algorithms.get(name).getMethod(name, class<?>[]) ```
+    5) J'invoque la méthode récupéré avec les arguments ``` m.invoke(null, arguments) ```
+
+ */
+
+
+
 /**
  * This class is Implemented with the design Pattern Singleton This class is
  * Managing the execution of algorithms processing images.
