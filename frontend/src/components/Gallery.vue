75266d65bf6077af5b48d3aea89b7130f4b9289c
Bastien Soucasse <bastienlouisclementsoucasse@gmail.com>
2021-03-25 23:52:44 +0100

    THIS FIXED EVERYTHING AND NOW WE'RE DONE :D

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -2,35 +2,35 @@
     <div class="gallery">
         <div class="gallery-wrapper">
             <div
-                class="gallery-content"
-                v-for="image in response"
                 :key="image.id"
-                :@click="$emit('LoadPreview', image.id)"
+                v-for="image in response"
+                v-on:click="loadPreview(image.id, image.name)"
+                class="gallery-image"
             >
-                <img class="gallery-image" :src="'/images/' + image.id" />
+                <img :src="'/images/' + image.id" />
             </div>
         </div>
+
+        <preview
+            :id="preview"
+            :name="name"
+            @close="closePreview"
+            v-if="preview != -1"
+        ></preview>
     </div>
 </template>
 
 <script>
+import Preview from "@/components/Preview.vue";
 import axios from "axios";
 
 export default {
     name: "Gallery",
 
-
-
-    emits: {
-        'LoadPreview': Number,
+    components: {
+        Preview,
     },
 
-    data() {
-        return {
-            response: [],
-            errors: [],
-        };
-    },
     methods: {
         callRestService() {
             axios
@@ -42,103 +42,34 @@ export default {
                     this.errors.push(e);
                 });
         },
-        /*preview(id) {
-            const app = document.getElementById("app");
 
-            let pBackground = document.createElement("div");
-            pBackground.classList.add("preview-background");
-            app.append(pBackground);
+        loadPreview(id, name) {
+            this.name = name;
+            this.preview = Number(id);
+        },
 
-            let pWindow = document.createElement("div");
-            pWindow.classList.add("preview-window");
-            app.append(pWindow);
-
-            pWindow.innerHTML = `
-                <div class="preview-header">
-                    <button class="preview-link" onclick="document.querySelector('.preview-window').style.gridTemplateColumns='100fr 350px';document.querySelector('.preview-features').style.display='block';"><span class="material-icons">edit</span></button>
-                    <button class="preview-link" onclick="alert('Cette fonctionnalité est encore à implémenter.');"><span class="material-icons">download</span></button>
-                    <button class="preview-link" onclick="alert('Cette fonctionnalité est encore à implémenter.');"><span class="material-icons">delete</span></button>
-                    <button class="preview-link" onclick="document.querySelector('.preview-background').remove();document.querySelector('.preview-window').remove();"><span class="material-icons">close</span></button>
-                </div>
-                <div class="preview-visual">
-                    <img class="preview-image" src="/images/` + id + `"
-                        onerror="document.querySelector('.preview-background').remove();document.querySelector('.preview-window').remove();" />
-                </div>
-                <div class="preview-features">
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Convert to grayscale</b>
-                        <input type="hidden" name="algorithm" value="toGrayscale" />
-                        <div class="preview-actions">
-                            <input class="preview-button" type="submit" value="To grayscale" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Apply a gain to the brightness</b>
-                        <input type="hidden" name="algorithm" value="changeBrightness" />
-                        <div class="preview-actions">
-                            <input class="preview-input" type="number" name="gain" min="-255" max="255" required />
-                            <input class="preview-button" type="submit" value="Change brightness" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Colorize by setting the hue</b>
-                        <input type="hidden" name="algorithm" value="colorize" />
-                        <div class="preview-actions">
-                            <input class="preview-input" type="number" name="hue" min="0" max="360" required />
-                            <input class="preview-button" type="submit" value="Colorize" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Extend the dynamics</b>
-                        <input type="hidden" name="algorithm" value="extendDynamics" />
-                        <div class="preview-actions">
-                            <input class="preview-button" type="submit" value="Extend dynamics" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Equalize the saturation or brightness histogram</b>
-                        <input type="hidden" name="algorithm" value="equalizeHistogram" />
-                        <div class="preview-actions">
-                            <div>
-                                <label><input class="preview-radio" type="radio" name="channel" value="1" checked />Saturation</label><br />
-                                <label><input class="preview-radio" type="radio" name="channel" value="2" />Brightness</label><br />
-                            </div>
-                            <input class="preview-button" type="submit" value="Equalize histogram" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Mean filter</b>
-                        <input type="hidden" name="algorithm" value="meanFilter" />
-                        <div class="preview-actions">
-                            <input class="preview-input" type="number" name="radius" min="1" max="10" required />
-                            <input class="preview-button" type="submit" value="Apply filter" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Gaussian filter</b>
-                        <input type="hidden" name="algorithm" value="gaussianFilter" />
-                        <div class="preview-actions">
-                            <input class="preview-input" type="number" name="radius" min="1" max="10" required />
-                            <input class="preview-button" type="submit" value="Apply filter" />
-                        </div>
-                    </form>
-                    <form class="preview-feature" method="get" action="/images/` + id + `">
-                        <b>Sobel operator</b>
-                        <input type="hidden" name="algorithm" value="sobelOperator" />
-                        <div class="preview-actions">
-                            <input class="preview-button" type="submit" value="Apply operator" />
-                        </div>
-                    </form>
-                </div>
-            `;
-        },*/
+        closePreview() {
+            this.name = "";
+            this.preview = -1;
+        },
     },
+
+    data() {
+        return {
+            response: [],
+            errors: [],
+            preview: Number,
+            name: String,
+        };
+    },
+
     mounted() {
+        this.name = "";
+        this.preview = -1;
         this.callRestService();
     },
 };
 </script>
-
 
 <style scoped>
 .gallery-wrapper {
@@ -152,7 +83,7 @@ export default {
     margin: auto;
 }
 
-.gallery-content {
+.gallery-image {
     display: block;
     height: 120px;
     width: 120px;
@@ -166,12 +97,12 @@ export default {
     cursor: pointer;
 }
 
-.gallery-content:hover {
+.gallery-image:hover {
     box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
     transform: scale(1.04);
 }
 
-.gallery-image {
+.gallery-image > img {
     width: 100%;
     height: 100%;
     object-fit: cover;
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,46 +3,21 @@
         <header>
             <h1>Gallery</h1>
         </header>
-        <component v-bind:is="currentComponent" @load-preview="loadPreview" @close-preview="closePreview" />
-        <Tools />
+
+        <gallery></gallery>
+
         <footer></footer>
     </div>
 </template>
 
 <script>
 import Gallery from "@/components/Gallery.vue";
-import Tools from "@/components/Tools.vue";
-import ImagePreview from "@/components/ImagePreview.vue";
 
 export default {
     name: "Home",
+
     components: {
         Gallery,
-        Tools,
-        ImagePreview,
-    },
-    data() {
-        return{
-            currentComponent: undefined,
-
-        };
-    },
-
-    methods :{
-        loadPreview(id){
-            this.currentComponent = ImagePreview;
-            ImagePreview.id = id;
-        },
-        closePreview(){
-            this.currentComponent = Gallery;
-        }
-    },
-
-    mounted() {
-        console.log(this.currentComponent);
-        this.currentComponent = Gallery;
-        console.log(this.currentComponent);
-
     },
 };
 </script>
